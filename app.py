663cd3fbd1123a976f70fd9e25cfc22eb8eabfe5
Abdullahi Khalif <abdullajust2021@gmail.com>
2024-06-06 19:35:21 +0300

    Added: Jupterfile

--- app.py
+++ app.py
@@ -1,47 +1,56 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open('models/popular.pkl','rb'))
-pt = pickle.load(open('models/pt.pkl','rb'))
-books = pickle.load(open('models/books.pkl','rb'))
-similarity_scores = pickle.load(open('models/similarity_scores.pkl','rb'))
+# Load models and data
+popular_df = pickle.load(open('models/popular.pkl', 'rb'))
+pt = pickle.load(open('models/pt.pkl', 'rb'))
+books = pickle.load(open('models/books.pkl', 'rb'))
+similarity_scores = pickle.load(open('models/similarity_scores.pkl', 'rb'))
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
+                           book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
-                           )
+                           rating=list(popular_df['avg_rating'].values))
+
 
 @app.route('/recommend')
 def recommend_ui():
     return render_template('recommend.html')
 
-@app.route('/recommend_books',methods=['post'])
+
+@app.route('/recommend_books', methods=['POST'])
 def recommend():
     user_input = request.form.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+    try:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
 
-    data = []
-    for i in similar_items:
-        item = []
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
 
-        data.append(item)
+            data.append(item)
 
-    print(data)
+        if not data:
+            return render_template('recommend.html', data=None, message="Not exist in the training dataset, the system cannot find similar books.")
 
-    return render_template('recommend.html',data=data)
+        return render_template('recommend.html', data=data)
+
+    except IndexError:
+        return render_template('recommend.html', data=None, message="Not exist in the training dataset, the system cannot find similar books.")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
